545a4949bd3725a1f0d5151ec722e58a3a587fae
asif-shahriar11 <asif.asr11@gmail.com>
2023-12-24 02:50:52 +0600

    A* completed

--- Offline-01-A-Star-Search/Code/Main.java
+++ Offline-01-A-Star-Search/Code/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
@@ -33,153 +35,153 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int k = input.nextInt();
-        input.nextLine();
+        String filename = "input.txt";
+        Scanner input;
+        try {
+            input = new Scanner(new File(filename));
+            int k = Integer.parseInt(input.nextLine());
+            //input.nextLine();
 
-        int[][] arr = new int[k+1][k+1];
-        int[][] goalArr = new int[k+1][k+1];
-        int[] arr1D = new int[k*k+1];
-        String line = input.nextLine();
-        String[] words = line.split(" ");
-        int count = 0;
-        for(int i=1; i<arr.length; i++) {
-            for(int j=1; j< arr.length; j++) {
-                String s = words[count++];
-                if(s.equalsIgnoreCase("*")) arr[i][j] = -1;
-                else arr[i][j] = Integer.parseInt(s);
-                if(count == k*k)    goalArr[i][j] = -1;
-                else goalArr[i][j] = count;
-                arr1D[count] = arr[i][j];
+            int[][] arr = new int[k+1][k+1];
+            int[][] goalArr = new int[k+1][k+1];
+            int[] arr1D = new int[k*k+1];
+            String line = input.nextLine();
+            String[] words = line.split(" ");
+            int count = 0;
+            for(int i=1; i<arr.length; i++) {
+                for(int j=1; j< arr.length; j++) {
+                    String s = words[count++];
+                    if(s.equalsIgnoreCase("*")) arr[i][j] = -1;
+                    else arr[i][j] = Integer.parseInt(s);
+                    if(count == k*k)    goalArr[i][j] = -1;
+                    else goalArr[i][j] = count;
+                    arr1D[count] = arr[i][j];
+                }
             }
-        }
 
-        Board initialBoard = new Board(k, arr);
-        Board finalBoard = new Board(k, goalArr);
-        Node initialNode = new Node(initialBoard,  finalBoard, null, true);
+            Board initialBoard = new Board(k, arr);
+            Board finalBoard = new Board(k, goalArr);
+            Node initialNode = new Node(initialBoard,  finalBoard, null, true);
 
-        // initialBoard.printBoard();
-        // finalBoard.printBoard();
-        boolean solvable = isSolvable(arr, arr1D, k*k);
-        if(!solvable) System.out.println("Given puzzle is not solvable.");
-        else {
-            System.out.println("Given puzzle is solvable.");
-            System.out.println("===========================================================");
+            // initialBoard.printBoard();
+            // finalBoard.printBoard();
+            boolean solvable = isSolvable(arr, arr1D, k*k);
+            if(!solvable) System.out.println("Given puzzle is not solvable.");
+            else {
+                System.out.println("Given puzzle is solvable.");
+                System.out.println("===========================================================");
 
 
-            int running = 0;
-            int hamCost = 0, manCost = 0;
-            int exploredNodesHam = 0, expandedNodesHam = 0, exploredNodesMan = 0, expandedNodesMan = 0;
-            while(running <= 1) {
-                if(running == 0) {
-                    // Hamming
-                    System.out.println("--------------Using Hamming Priority Function-----------------");
-                    initialNode = new Node(initialBoard,  finalBoard, null, true);
-                    //System.out.println(initialNode.getF());
+                int running = 0;
+                int hamCost = 0, manCost = 0;
+                int exploredNodesHam = 0, expandedNodesHam = 0, exploredNodesMan = 0, expandedNodesMan = 0;
+                while(running <= 1) {
+                    if(running == 0) {
+                        // Hamming
+                        System.out.println("--------------Using Hamming Priority Function-----------------");
+                        initialNode = new Node(initialBoard,  finalBoard, null, true);
+                        //System.out.println(initialNode.getF());
 
-                    PriorityQueue<Node> pq = new PriorityQueue<>();
-                    ArrayList<Node> closedList = new ArrayList<>();
-                    pq.add(initialNode);
-                    //pq.poll().getBoard().printBoard();
+                        PriorityQueue<Node> pq = new PriorityQueue<>();
+                        ArrayList<Node> closedList = new ArrayList<>();
+                        pq.add(initialNode);
+                        //pq.poll().getBoard().printBoard();
 
-                    expandedNodesHam = 1; // entered the queue
-                    exploredNodesHam = 0; // exited the queue
+                        expandedNodesHam = 1; // entered the queue
+                        exploredNodesHam = 0; // exited the queue
 
-                    while(true) {
-                        Node node = pq.poll();
-                        exploredNodesHam++;
-                        closedList.add(node);
-                        node.getBoard().printBoard();
-                        if(node.getBoard().isSameBoard(finalBoard)) {
-                            hamCost = node.getG();
-                            break;
-                        }
-                        ArrayList<Node> neighbors = node.getNeighbors(true);
-                        for(Node neighNode:neighbors) {
-                            boolean inClosedList = false;
-                            for(Node tempNode:closedList) {
-                                if(neighNode.getBoard().isSameBoard(tempNode.getBoard())) {
-                                    inClosedList = true;
+                        while(true) {
+                            Node node = pq.poll();
+                            exploredNodesHam++;
+                            closedList.add(node);
+                            node.getBoard().printBoard();
+                            if(node.getBoard().isSameBoard(finalBoard)) {
+                                hamCost = node.getG();
+                                break;
+                            }
+                            ArrayList<Node> neighbors = node.getNeighbors(true);
+                            for(Node neighNode:neighbors) {
+                                boolean inClosedList = false;
+                                for(Node tempNode:closedList) {
+                                    if(neighNode.getBoard().isSameBoard(tempNode.getBoard())) {
+                                        inClosedList = true;
+                                    }
+                                }
+                                if(!inClosedList) {
+                                    pq.add(neighNode);
+                                    expandedNodesHam++;
                                 }
                             }
-                            if(!inClosedList) {
-                                pq.add(neighNode);
-                                expandedNodesHam++;
-                            }
+
                         }
+
+                        System.out.println();
 
                     }
 
-                    System.out.println();
+                    else {
+                        // Manhattan
+                        System.out.println("--------------Using Manhattan Priority Function-----------------");
+                        initialNode = new Node(initialBoard, finalBoard, null, false);
+                        //System.out.println(initialNode.getF());
 
-                }
+                        PriorityQueue<Node> pq1 = new PriorityQueue<>();
+                        ArrayList<Node> closedList = new ArrayList<>();
+                        pq1.add(initialNode);
 
-                else {
-                    // Manhattan
-                    System.out.println("--------------Using Manhattan Priority Function-----------------");
-                    initialNode = new Node(initialBoard, finalBoard, null, false);
-                    //System.out.println(initialNode.getF());
+                        expandedNodesMan = 1; // entered the queue
+                        exploredNodesMan = 0; // exited the queue
 
-                    PriorityQueue<Node> pq1 = new PriorityQueue<>();
-                    ArrayList<Node> closedList = new ArrayList<>();
-                    pq1.add(initialNode);
+                        while(true) {
+                            Node node = pq1.poll();
+                            exploredNodesMan++;
+                            closedList.add(node);
+                            node.getBoard().printBoard();
+                            if(node.getBoard().isSameBoard(finalBoard)) {
+                                manCost = node.getG();
+                                break;
+                            }
 
-                    expandedNodesMan = 1; // entered the queue
-                    exploredNodesMan = 0; // exited the queue
+                            ArrayList<Node> neighbors = node.getNeighbors(false);
 
-                    while(true) {
-                        Node node = pq1.poll();
-                        exploredNodesMan++;
-                        closedList.add(node);
-                        node.getBoard().printBoard();
-                        if(node.getBoard().isSameBoard(finalBoard)) {
-                            manCost = node.getG();
-                            break;
-                        }
-
-                        ArrayList<Node> neighbors = node.getNeighbors(false);
-
-                        for(Node neighNode:neighbors) {
-                            boolean inClosedList = false;
-                            for(Node tempNode:closedList) {
-                                if(neighNode.getBoard().isSameBoard(tempNode.getBoard())) {
-                                    inClosedList = true;
+                            for(Node neighNode:neighbors) {
+                                boolean inClosedList = false;
+                                for(Node tempNode:closedList) {
+                                    if(neighNode.getBoard().isSameBoard(tempNode.getBoard())) {
+                                        inClosedList = true;
+                                    }
+                                }
+                                if(!inClosedList) {
+                                    pq1.add(neighNode);
+                                    expandedNodesMan++;
                                 }
                             }
-                            if(!inClosedList) {
-                                pq1.add(neighNode);
-                                expandedNodesMan++;
-                            }
+
+
                         }
 
 
                     }
 
-
+                    running++;
                 }
 
-                running++;
+                System.out.println("--------------------Using Hamming priority function-----------------");
+                System.out.println("Cost: " + hamCost);
+                System.out.println("Explored Nodes: " + exploredNodesHam);
+                System.out.println("Expanded Nodes: " + expandedNodesHam);
+                System.out.println();
+                System.out.println("--------------------Using Manhattan priority function-----------------");
+                System.out.println("Cost: " + manCost);
+                System.out.println("Explored Nodes: " + exploredNodesMan);
+                System.out.println("Expanded Nodes: " + expandedNodesMan);
             }
 
-            System.out.println("--------------------Using Hamming priority function-----------------");
-            System.out.println("Cost: " + hamCost);
-            System.out.println("Explored Nodes: " + exploredNodesHam);
-            System.out.println("Expanded Nodes: " + expandedNodesHam);
-            System.out.println();
-            System.out.println("--------------------Using Manhattan priority function-----------------");
-            System.out.println("Cost: " + manCost);
-            System.out.println("Explored Nodes: " + exploredNodesMan);
-            System.out.println("Expanded Nodes: " + expandedNodesMan);
-
-
-
-
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-
-
-
-
-
-
+        
+        
     }
 }
